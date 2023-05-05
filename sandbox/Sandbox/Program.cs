58537b4a60308b6f4b9bf787f841913ef3e269fe
grahamssendagire <ssendagiregraham745@gmail.com>
2023-05-05 13:46:04 -0700

    k

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.Write("what is your favorite color?");
+        string color = Console.ReadLine();
+        Console.WriteLine(color);
+        
     }
 }
